6e7f9a649f603fce6ebbb096f33a0e0c0343dd14
Gerald <gsalazarq@uni.pe>
2023-04-04 08:22:13 -0500

    primer commit para actualizar el código de Colombia en Octave

---
+++ distance_calculation/elevation_raster.m
@@ -0,0 +1,12 @@
+% load data from raster text file 
+data = load('/home/justus/Dropbox/b_research/b-muongraphy-UNI/b_cosmicraysuni/atenuation_calculation/rastert_dem_uni1-clean.txt');
+raster = reshape(data, 317, 626)
+imagesc(raster)
+[n,m] = meshgrid(1:size(data,2), 1:size(data,1));
+surf(n,m,raster, 'EdgeColor', 'none');
+colormap(jet); 
+colorbar; 
+xlabel('X'); 
+ylabel('Y'); 
+zlabel('Data'); 
+rotate3d on;
