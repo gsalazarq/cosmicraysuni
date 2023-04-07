f9883a217d43d3f45022915513a4f8576f9127df
Gerald <gsalazarq@uni.pe>
2023-04-07 10:25:47 -0500

    gráfica del raster en toda su extensión

--- distance_calculation/elevation_raster.m
+++ distance_calculation/elevation_raster.m
@@ -1,8 +1,9 @@
 % load data from raster text file 
 data = load('/home/justus/Dropbox/b_research/b-muongraphy-UNI/b_cosmicraysuni/atenuation_calculation/rastert_dem_uni1-clean.txt');
-raster = reshape(data, 317, 626)
-imagesc(raster)
-[n,m] = meshgrid(1:size(data,2), 1:size(data,1));
+raster = reshape(data, 317, 626);
+imagesc(raster);
+[n,m] = meshgrid(1:size(data,2), 1:size(data,1)); % create x and y vectors using meshgrid
+
 surf(n,m,raster, 'EdgeColor', 'none');
 colormap(jet); 
 colorbar; 
